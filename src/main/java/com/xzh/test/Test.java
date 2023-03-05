407ce8eaa23d21337500d7ac4eb40b21bafb279e
xzh <ixiezhihong@qq.com>
2023-03-05 23:16:19 +0800

    hot-fix commit2

--- src/main/java/com/xzh/test/Test.java
+++ src/main/java/com/xzh/test/Test.java
@@ -9,7 +9,7 @@ public class Test {
         System.out.println("Hello, World");
         System.out.println("Hello,World!!!!");
         System.out.println("Hello,World!!!!");
-
+        System.out.println("4");
     }
 
 }
